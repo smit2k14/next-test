e0333a161b9e04dc6a17b3ba359af58dc87edd55
Smit Shah <smir2k14@gmail.com>
2021-03-02 17:58:21 +0530

    chore: fix def

---
+++ pages/def/index.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Qwer = () => {
+  return (
+    <div>
+      <div>This is Def</div>
+      <Link href="/abc/qwer">
+        <a>Click here to go to qwer</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Qwer;
